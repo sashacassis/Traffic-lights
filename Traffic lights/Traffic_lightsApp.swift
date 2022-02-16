e21302e92a97efb1cf65cfd1098743f3673b2f74
sasha.cassis <alexander.dorogoff@icloud.com>
2022-02-16 21:43:13 +0300

    Initial Commit

---
+++ Traffic lights/Traffic_lightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_lightsApp.swift
+//  Traffic lights
+//
+//  Created by Alexandr Dorogov on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_lightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
